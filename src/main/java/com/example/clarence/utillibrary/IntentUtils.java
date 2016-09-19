96a0ffdc4ae1f400532ed41e97f3b4da3e3a4e6e
caishuxing <caishuxing@linggan.com>
2016-09-19 15:32:19 +0800

    添加工具

---
+++ src/main/java/com/example/clarence/utillibrary/IntentUtils.java
@@ -0,0 +1,24 @@
+package com.example.clarence.utillibrary;
+
+import android.content.Intent;
+
+/**
+ * Created by clarence on 16/9/19.
+ */
+public class IntentUtils {
+    /**
+     * Get the Intent for selecting content to be used in an Intent Chooser.
+     *
+     * @return The intent for opening a file with Intent.createChooser()
+     * @author paulburke
+     */
+    public static Intent createGetContentIntent() {
+        // Implicitly allow the user to select a particular kind of data
+        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        // The MIME data type filter
+        intent.setType("*/*");
+        // Only return URIs that can be opened with ContentResolver
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        return intent;
+    }
+}
