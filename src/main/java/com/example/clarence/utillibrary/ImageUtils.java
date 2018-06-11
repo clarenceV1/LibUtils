3a7c6d09dade710ad1c50235034b2c644d91698f
caishuxing <caishuxing@linggan.com>
2018-06-11 23:24:10 +0800

    添加图片裁剪功能

---
+++ src/main/java/com/example/clarence/utillibrary/ImageUtils.java
@@ -0,0 +1,29 @@
+package com.example.clarence.utillibrary;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.lang.ref.WeakReference;
+
+public class ImageUtils {
+
+    public static Bitmap getBitmap(String path, int w, int h) {
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true;
+        opts.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        BitmapFactory.decodeFile(path, opts);
+        int width = opts.outWidth;
+        int height = opts.outHeight;
+        float scaleWidth = 0.f, scaleHeight = 0.f;
+        if (width > w || height > h) {
+            scaleWidth = ((float) width) / w;
+            scaleHeight = ((float) height) / h;
+        }
+        opts.inJustDecodeBounds = false;
+        float scale = Math.max(scaleWidth, scaleHeight);
+        opts.inSampleSize = (int) scale;
+        WeakReference<Bitmap> weak = new WeakReference<Bitmap>(
+                BitmapFactory.decodeFile(path, opts));
+        return Bitmap.createScaledBitmap(weak.get(), w, h, true);
+    }
+}
