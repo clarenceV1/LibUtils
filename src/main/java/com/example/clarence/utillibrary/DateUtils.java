cb2ae4f15162e58b05187363625916100e93c92e
caishuxing <caishuxing@linggan.com>
2018-07-02 00:01:52 +0800

    添加工具

--- src/main/java/com/example/clarence/utillibrary/DateUtils.java
+++ src/main/java/com/example/clarence/utillibrary/DateUtils.java
@@ -1,7 +1,9 @@
 package com.example.clarence.utillibrary;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class DateUtils {
@@ -33,5 +35,37 @@ public class DateUtils {
         return formatDate;
     }
 
+    /**
+     * 时间戳转换成日期格式字符串
+     *
+     * @param seconds 精确到秒的字符串
+     * @return
+     */
+    public static String timeStamp2Date(String seconds, String format) {
+        if (seconds == null || seconds.isEmpty() || seconds.equals("null")) {
+            return "";
+        }
+        if (format == null || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+    }
 
+    /**
+     * 日期格式字符串转换成时间戳
+     *
+     * @param date_str 字符串日期
+     * @param format   如：yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static long date2TimeStamp(String date_str, String format) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            return sdf.parse(date_str).getTime();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
