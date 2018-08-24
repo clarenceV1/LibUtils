5ee1b4501aa4089bf2ae83bb4fdda98ffd51083f
caishuxing <caishuxing@linggan.com>
2018-08-24 15:39:15 +0800

    更新

--- src/main/java/com/example/clarence/utillibrary/DateUtils.java
+++ src/main/java/com/example/clarence/utillibrary/DateUtils.java
@@ -70,7 +70,7 @@ public class DateUtils {
         return 0;
     }
 
-    public static String[] WEEK = {"星期天","星期一","星期二","星期三","星期四","星期五","星期六"};
+    public static String[] WEEK = {"星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
     public static final int WEEKDAYS = 7;
 
     public static String dateToWeek(long time) {
